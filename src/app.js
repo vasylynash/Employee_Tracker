9d65e7bcb73f3881ae506406c0bd4ec298969dd2
Vasylyna Shevadutska <vaskash@gmail.com>
2021-06-15 16:12:19 -0700

    Updated formatting for roles view

--- src/app.js
+++ src/app.js
@@ -1,92 +1,20 @@
-const mysql = require('mysql');
 const inquirer = require('inquirer');
-const cTable = require("console.table");
-const employeeDAO = require('./employee_dao');
+const cTable = require('console.table');
 const mysqlConnection = require('./mysql_connection');
-const departmentDAO = require('./deparment_dao');
 const Department = require("./department");
 const Role = require("./role");
-const roleDAO = require("./role_dao");
 const employee_dao = require('./employee_dao');
 const Employee = require('./employee');
-// const employeeQuestions = require('./questions').addEmployeeQuestions;
 const initialChoice = require("./questions").initialChoice;
 const role_dao = require('./role_dao');
-// const { addEmployeeQuestions, deleteEmployeeSelection, updateEmployeeRoleSelection, updateRoleSelection } = require('./questions');
 const reports_dao = require('./reports_dao');
 const questions = require("./questions");
-const { selectRole } = require('./questions');
 const deparment_dao = require('./deparment_dao');
-// const Employee = require("./employee");
-
-// (async () => {
-// console.log(JSON.stringify(await employeeDAO.getAll()));
-// const role = new Role();
-// role.title = "Manager";
-// role.salary = 120, 000;
-// role.departmentId
-// department.name = "Sales2";
-// department.id = 2;
-// await departmentDAO.delete(department);
-// const employee = await employee_dao.getById(1);
-// console.log(employee);
-// const manager = await employee.getManager();
-// console.log(manager);
-//     const employee = new Employee();
-//     employee.firstName = "Jane",
-//         employee.lastName = "Bautina",
-//         employee.roleId = 1;
-//     employee.managerId = 1;
-//     employee.id = 3;
-//     // await employee_dao.save(employee);
-//     console.log(await employee_dao.getAll());
-//     await employee_dao.delete(employee);
-//     console.log(await employee_dao.getAll());
-//     mysqlConnection.end();
-//     console.log(await role_dao.getAll());
-// })();
-
-// inquirer
-//     .prompt(employeeQuestions)
-//     .then(async (answers) => {
-//         console.log(answers);
-//         let title = answers.role;
-//         let id = await role_dao.getId(title);
-//         console.log(id);
-//         let names = answers.manager.split(" ");
-//         let managerId = await employee_dao.getId(names[0], names[1]);
-//         console.log(managerId);
-//         // const employee = new Employee();
-//         let employee = {
-//             firstName: answers.firstName,
-//             lastName: answers.lastName,
-//             roleId: id,
-//             managerId: managerId,
-//         }
-//         console.log(employee);
-//     })
 
 class App {
-
-    // addEmployee() {
-    //     inquirer.prompt(addEmployeeQuestions)
-    //         .then(async (answers) => {
-    //             const employee = new Employee();
-    //             employee.firstName = answers.firstName;
-    //             employee.lastName = answers.lastName;
-    //             employee.setRole(answers.role);
-    //             employee.setManager(answers.manager);
-    //             await employee_dao.save(employee);
-    //             console.table(await reports_dao.viewAll());
-    //             this.mainMenu();
-    //         })
-    // }
-
-
     async mainMenu() {
         while (true) {
             let answer = await inquirer.prompt(initialChoice);
-
             switch (answer.initialChoice) {
                 case "Add employee":
                     await this.addEmployee();
@@ -118,7 +46,9 @@ class App {
                     await this.createRole();
                     break;
                 case "View roles":
-                    console.table(await role_dao.getAll());
+                    console.table(["ID", "Title", "Salary"], (await role_dao.getAll()).map(role => {
+                        return [role.id, role.title, role.salary]
+                    }));
                     break;
                 case "Delete a role":
                     await this.deleteRole();
